9bac57e8141cec9bfdb8df70040bed00afd6a5ba
geoffvargo <geoff.vargo@stonybrook.edu>
2022-01-23 02:38:29 -0500

    added unit tests

---
+++ src/test/java/com/geoffvargo/CoderByte/LongestIncreasingSequence/MainTest.java
@@ -0,0 +1,36 @@
+package com.geoffvargo.CoderByte.LongestIncreasingSequence;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+	
+	@org.junit.jupiter.api.Test
+	void longestIncreasingSequence_TEST1() {
+		int[] arr = {10, 12, 4, 6, 100, 2, 56, 34, 79};
+		assertEquals(4, Main.LongestIncreasingSequence(arr));
+	}
+	
+	@org.junit.jupiter.api.Test
+	void longestIncreasingSequence_TEST2() {
+		int[] arr = {10, 12, 4, 6, 100, 2, 56, 34, 79, 45, 33, 12, 45, 67, 89};
+		assertEquals(6, Main.LongestIncreasingSequence(arr));
+	}
+	
+	@org.junit.jupiter.api.Test
+	void longestIncreasingSequence_TEST3() {
+		int[] arr = {9, 9, 4, 2};
+		assertEquals(1, Main.LongestIncreasingSequence(arr));
+	}
+	
+	@org.junit.jupiter.api.Test
+	void longestIncreasingSequence_TEST4() {
+		int[] arr = {4, 3, 5, 1, 6};
+		assertEquals(3, Main.LongestIncreasingSequence(arr));
+	}
+	
+	@org.junit.jupiter.api.Test
+	void longestIncreasingSequence_TEST5() {
+		int[] arr = {10, 22, 9, 33, 21, 50, 41, 60, 22, 68, 90};
+		assertEquals(7, Main.LongestIncreasingSequence(arr));
+	}
+}
